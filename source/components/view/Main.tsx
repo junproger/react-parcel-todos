c374702b1843022dbaa2d918d450677468410921
Junior Programmer <progphyton@gmail.com>
2023-09-17 18:02:33 +0300

    feat: add state for todos

--- source/components/view/Main.tsx
+++ source/components/view/Main.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import styles from './main.module.css';
 
@@ -6,10 +6,24 @@ import { Join } from '../control/Join';
 import { Control } from '../control/Control';
 import { Todos } from '../view/Todos';
 
+import { ITodo } from '../../types/ITodo';
+import { idkey } from '../../helpers/idkey';
+
 export const Main: FC = () => {
+  const [getTodos, setTodos] = useState<ITodo[]>([]);
   const addTodo = (value: string): void => {
+    setTodos((prev) => {
+      return [
+        ...prev,
+        {
+          id: idkey(value),
+          todo: value,
+          complete: false,
+        },
+      ];
+    });
     // eslint-disable-next-line no-console
-    return console.log(value);
+    return console.log(getTodos);
   };
   return (
     <main className={styles['main']}>
