3aaeb2ed392239ee3425f353206e8d1059578189
Junior Programmer <progphyton@gmail.com>
2023-09-24 17:42:37 +0300

    refactor: join component with onChangeHandler

--- source/components/control/Join.tsx
+++ source/components/control/Join.tsx
@@ -1,4 +1,4 @@
-import { FC, KeyboardEvent, useState } from 'react';
+import { ChangeEvent, FC, KeyboardEvent, useState } from 'react';
 
 import * as styles from './join.module.css';
 
@@ -8,7 +8,16 @@ export interface JoinProp {
 
 export const Join: FC<JoinProp> = ({ callback }) => {
   const [getValue, setValue] = useState('');
-  const keyHandler = (e: KeyboardEvent<HTMLInputElement>): void => {
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value;
+    if (value.trim()) {
+      setValue(value);
+    }
+    if (!value.trim()) {
+      setValue('');
+    }
+  };
+  const keyDownHandler = (e: KeyboardEvent<HTMLInputElement>): void => {
     if (e.key === 'Enter' && getValue.trim()) {
       callback(getValue.trim());
       setValue('');
@@ -26,8 +35,8 @@ export const Join: FC<JoinProp> = ({ callback }) => {
         className={styles['input']}
         aria-label="What needs to be done?"
         placeholder="What needs to be done?"
-        onChange={(e): void => setValue(e.target.value)}
-        onKeyDown={keyHandler}
+        onChange={onChangeHandler}
+        onKeyDown={keyDownHandler}
       />
     </div>
   );
